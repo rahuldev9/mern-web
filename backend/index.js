8562dc6a92b11af36324e7bf6e94356a4688dfcc
rahuldev9 <webdeveloperaim@gmail.com>
2024-12-27 16:15:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,11 @@ const path = require("path");
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],s
+    credentials: true
+}));
 
 
 app.get('/',(req,res)=>{
